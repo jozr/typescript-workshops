76c3e0cd9ec4a1d38b3eebbeeb09d1c47a8afd23
rufael negash <rufael.negash@gmail.com>
2020-05-07 11:10:51 +0200

    remove build files

--- build/basic-types/boolean.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/*
-The most basic datatype is the simple true/false value, which JavaScript and
-TypeScript call a boolean value.
-*/
-const isDone = false;
-//# sourceMappingURL=boolean.js.map
--- build/two-fer/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class TwoFer {
-    static twoFer( /* Parameters go here */) {
-        // Your code here
-    }
-}
-exports.default = TwoFer;
-//# sourceMappingURL=index.js.map
